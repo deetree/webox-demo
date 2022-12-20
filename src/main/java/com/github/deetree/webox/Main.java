84a8ee72ebf6b284a6c40aa317d1143a36a5e022
Mariusz <49595495+deetree@users.noreply.github.com>
2022-12-20 15:33:41 +0100

    Initial commit

---
+++ src/main/java/com/github/deetree/webox/Main.java
@@ -0,0 +1,10 @@
+package com.github.deetree.webox;
+
+/**
+ * @author mariusz
+ */
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/github/deetree/webox/MainTest.java
@@ -0,0 +1,16 @@
+package com.github.deetree.webox;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+@Test
+public class MainTest {
+
+    public void shouldPassSampleTest() {
+        //g
+        //w
+        //t
+        assertTrue(true);
+    }
+}
